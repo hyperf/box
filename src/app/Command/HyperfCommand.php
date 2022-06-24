4d6459bff61fbd015440a909f6da5270b4914455
huangzhhui <huangzhwork@gmail.com>
2022-06-25 03:50:10 +0800

    Add hyperf proxy command
    Signed-off-by: huangzhhui <huangzhwork@gmail.com>

---
+++ src/app/Command/HyperfCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Config;
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Utils\Str;
+use Psr\Container\ContainerInterface;
+
+#[Command]
+class HyperfCommand extends AbstractCommand
+{
+
+    protected Config $config;
+
+    public function __construct(protected ContainerInterface $container)
+    {
+        parent::__construct('hyperf');
+        $this->config = $this->container->get(Config::class);
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this->setDescription('The hyperf proxy command.');
+        $this->ignoreValidationErrors();
+    }
+
+    public function handle()
+    {
+        $path = $this->config->getConfig('path.runtime', getenv('HOME') . '/.box');
+        $currentPhpVersion = $this->config->getConfig('versions.php', '8.1');
+        $hyperfBin = $this->config->getConfig('hyperf.bin', './bin/hyperf.php');
+        $bin = $path . '/php' . $currentPhpVersion . ' ' . $hyperfBin;
+        $command = Str::replaceFirst('hyperf ', '', (string)$this->input);
+        $fullCommand = sprintf('%s %s', $bin, $command);
+        $this->liveCommand($fullCommand);
+    }
+
+}
