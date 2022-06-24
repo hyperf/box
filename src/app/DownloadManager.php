3c6dcf747c17a1e1318d2357b34e5233f31da927
huangzhhui <huangzhwork@gmail.com>
2022-06-25 02:33:34 +0800

    Optimized
    Signed-off-by: huangzhhui <huangzhwork@gmail.com>

--- src/app/DownloadManager.php
+++ src/app/DownloadManager.php
@@ -40,7 +40,7 @@ class DownloadManager
 
     protected function createRuntimePath(): void
     {
-        $path = $this->config->getConfig('path.runtime', getenv('HOME') . '/box/runtime');
+        $path = $this->config->getConfig('path.runtime', getenv('HOME') . '/.box');
         if (! file_exists($path)) {
             mkdir($path, 0755);
             chmod($path, 0755);
